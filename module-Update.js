83e4ebb41935b7ff00872ac0c9100a8b67f683a3
2310993697 <82264369+2310993697@users.noreply.github.com>
2021-11-25 20:58:37 +0800

    Add files via upload

---
+++ module-Update.js
@@ -0,0 +1,190 @@
+// TODO: 显示更新弹框
+importClass(android.graphics.Color);
+importClass(android.graphics.drawable.GradientDrawable);
+importClass(android.view.LayoutInflater);
+importClass(android.widget.PopupWindow);
+importClass(android.view.ViewGroup);
+importClass(android.view.Gravity);
+importClass(android.view.animation.ScaleAnimation);
+importClass(android.view.animation.Animation);
+
+importClass(android.graphics.Path);
+importClass(android.graphics.RectF);
+importClass(android.animation.ObjectAnimator);
+importClass(android.animation.AnimatorListenerAdapter);
+importClass(android.util.TypedValue);
+importClass(android.graphics.PaintFlagsDrawFilter);
+
+let 增量更新按钮颜色 = "#009afe";
+let 文字颜色 = "#000000";
+
+var storage = storages.create("OPPO商城小铺");
+
+
+
+function Update() {
+    let view = ui.inflate(
+        <LinearLayout gravity="center">
+            <LinearLayout orientation="vertical" layout_width="310dp">
+                <card cardCornerRadius="6dp"  layout_width="match_parent" layout_height="wrap_content">
+                    <LinearLayout
+                    orientation="vertical"
+                    background="#ffffff"
+                    layout_width="match_parent"
+                    layout_height="wrap_content"
+                    padding="30 0 30 0"
+                    >
+                    <text
+                    margin="0 12 0 12"
+                    textStyle="bold"
+                    textColor="#008fff"
+                    textSize="18sp"
+                    text="欢太任务小铺"
+                    w="*"
+                    gravity="center"
+                    >
+                </text>
+                <text id="升级内容">
+                </text>
+                <progressbar id="_progress" marginBottom="-6" w="*" margin="0 12 0 12"style="@style/Base.Widget.AppCompat.ProgressBar.Horizontal" visibility="invisible" />
+                
+                <text
+                textSize="14sp"
+                textColor="#ffffff"
+                gravity="center"
+                id="更新安装"
+                paddingTop="10dp"
+                paddingBottom="10dp"
+                focusable="true"
+                layout_width="match_parent"
+                layout_height="wrap_content"
+                text="更新安装"
+                margin="0 0 0 12"
+                />
+            </LinearLayout>
+        </card>
+        </LinearLayout>
+        </LinearLayout>,
+        null,
+        false
+    );
+    
+    //监听脚本间广播'download'事件
+events.broadcast.on("download", function (X) {
+    switch (X.name) {
+        case "进度":
+            ui.run(() => {
+                view._progress.setProgress(0 + X.data);
+                view.更新安装.setText("下载中 " + X.data + "%");
+            })
+            break;
+        case "结果":
+            if (X.data == "下载完成") {
+              view.更新安装.setText("下载完成");
+            }
+            break;
+    }
+});
+
+    var 版本=storage.get('版本');
+    var 更新内容=storage.get('更新内容');
+    var 体积=storage.get('体积');
+    let 升级内容 = ["版本:"+版本, "增量包体积:"+体积, "", "更新内容:", 更新内容].join("\n");
+    view.升级内容.setText(升级内容);
+    //setBackgroundPureColour(view.更新安装);
+    view1 = view.更新安装;
+    setBackgroundRoundGradientColor(view1, "topLeftToBottomRight");
+    let mPopWindow = new PopupWindow(
+        view,
+        ViewGroup.LayoutParams.MATCH_PARENT,
+        ViewGroup.LayoutParams.WRAP_CONTENT,
+        true
+    );
+    mPopWindow.setFocusable(true);
+    mPopWindow.setOutsideTouchable(true);
+    mPopWindow.setContentView(view);
+
+    mPopWindow.setOnDismissListener(
+        new PopupWindow.OnDismissListener({
+            onDismiss: function() {
+                backgroundAlpha(0.99);
+            },
+        })
+    );
+    ui.run(function() {
+
+        mPopWindow.showAtLocation(activity.getWindow().getDecorView(), Gravity.CENTER, 0, 0);
+
+       backgroundAlpha(0.3);
+    });
+    view.更新安装.click(function() {
+        view._progress.attr("visibility", "visible");
+        view.更新安装.setText("下载中 0%");
+        engines.execScriptFile("./res/task/task-download.js");
+    });
+    
+   return mPopWindow;
+}
+module.exports = Update;
+// showUpdateDialog();
+
+/* ---------------------自定义函数----------------------------------------------------- */
+
+
+
+
+function backgroundAlpha(bgAlpha) {
+    let lp = activity.getWindow().getAttributes();
+    let 原来的透明度 = lp.alpha;
+    lp.alpha = bgAlpha; //0.0-1.0
+    let t = 300;
+    let distance = bgAlpha - 原来的透明度;
+    let num = t / 16;
+    let unit = distance / num;
+    threads.start(function() {
+        for (var i = 0; i < num; i++) {
+            ui.post(function() {
+                lp.alpha = 原来的透明度 + unit * (i + 1);
+                activity.getWindow().setAttributes(lp);
+            });
+            sleep(16);
+        }
+    });
+}
+
+
+
+function setBackgroundRoundGradientColor(view, directionType) {
+        let colorArr = util.java.array("int", 5);
+        colorArr[0] = colors.parseColor("#00b3ff");
+        colorArr[1] = colors.parseColor("#00aeff");
+        colorArr[2] = colors.parseColor("#008fff");
+        colorArr[3] = colors.parseColor("#008fff");
+        colorArr[4] = colors.parseColor("#008fff");
+        gradientDrawable = new GradientDrawable();
+        gradientDrawable.setShape(GradientDrawable.RECTANGLE);
+        gradientDrawable.setColors(colorArr); //添加颜色组
+        gradientDrawable.setCornerRadius(30);
+        gradientDrawable.setGradientType(GradientDrawable.LINEAR_GRADIENT); //设置线性渐变
+        switch (directionType) {
+            case "leftRight":
+                gradientDrawable.setOrientation(GradientDrawable.Orientation.LEFT_RIGHT); //设置渐变方向
+                break;
+            case "upDown":
+                gradientDrawable.setOrientation(GradientDrawable.Orientation.TOP_BOTTOM); //设置渐变方向
+                break;
+            case "topLeftToBottomRight":
+                // gradientDrawable.setOrientation(GradientDrawable.Orientation.TL_BR); //设置渐变方向
+                gradientDrawable.setOrientation(GradientDrawable.Orientation.TR_BL); //设置渐变方向
+                break;
+            case "RADIAL_GRADIENT":
+                gradientDrawable.setGradientType(GradientDrawable.RADIAL_GRADIENT); //设置半径渐变
+                gradientDrawable.setGradientRadius(330); //渐变的半径值
+                break;
+        }
+        gradientDrawable.setSize(100, 100);
+        view.setBackground(gradientDrawable);
+    }
+
+
+
---
+++ module-inform.js
@@ -0,0 +1,92 @@
+importClass("android.content.res.Resources");
+importClass("android.app.NotificationManager");
+importClass("android.app.Notification");
+importClass("android.app.PendingIntent");
+importClass(android.graphics.BitmapFactory);
+importClass(android.content.Context);
+importClass(android.net.Uri);
+importClass(java.io.FileInputStream);
+importClass(android.graphics.drawable.Icon);
+
+
+function inform(H, b, A) {
+    if (files.exists("/sdcard/1.png") == true) {
+        var intent = PendingIntent.getActivity(context, 0, app.intent({
+            action: "android.intent.action.MAIN",
+            packageName: "com.httask.sign",
+            className: "com.httask.sign.SplashActivity",
+            category: ["android.intent.category.LAUNCHER"],
+            flags: ["activity_new_task"]
+        }), 0);
+
+        var icon_path = "/sdcard/1.png";
+        var ic = ['ic_3d_rotation_black_48dp', 'ic_accessibility_black_48dp', 'ic_accessible_black_48dp', 'ic_account_balance_black_48dp', 'ic_account_balance_wallet_black_48dp', 'ic_account_box_black_48dp'];
+
+        var i = random(0, ic.length);
+        var icon = getResourceID(ic[i], "drawable");
+        var manager = context.getSystemService(android.app.Service.NOTIFICATION_SERVICE);
+        var notification;
+        if (device.sdkInt >= 26) {
+            var channel = new android.app.NotificationChannel("channel_id", "领取现金", android.app.NotificationManager.IMPORTANCE_DEFAULT);
+            channel.enableLights(true);
+            channel.setLightColor(0xff0000);
+            channel.setShowBadge(false);
+            manager.createNotificationChannel(channel);
+            notification = new android.app.Notification.Builder(context, "channel_id")
+                .setContentTitle("" + new Date())
+                .setContentText(b + ":" + A)
+                .setWhen(new Date().getTime())
+                .setSmallIcon(getIcon(icon_path).icon)
+                .setLargeIcon(getIcon(icon_path).icon)
+                .setOngoing(true)
+                .setContentIntent(intent)
+                .setTicker("欢太任务小铺")
+                .build();
+        } else {
+            notification = new android.app.Notification.Builder(context)
+                .setContentTitle("" + new Date())
+                .setContentText(b + ":" + A)
+                .setWhen(new Date().getTime())
+                .setSmallIcon(getIcon(icon_path).icon)
+                .setOngoing(true)
+                .setContentIntent(intent)
+                .setTicker("欢太任务小铺")
+                .build();
+        }
+        manager.notify(H, notification);
+    } else {
+        threads.start(function() {
+            下载图片();
+            通知(H, b, A);
+        })
+    }
+}
+module.exports = inform;
+
+function getResourceID(name, defType) {
+    //获取资源文件ID
+    //参数
+    //defType 类名 如drawable id string等
+    //name 资源名
+    var resource = context.getResources();
+    return resource.getIdentifier(name, defType, context.getPackageName());
+}
+
+function getIcon(icon_path, largeBitmap) {
+    largeBitmap = BitmapFactory.decodeStream(new FileInputStream(icon_path));
+    return {
+        icon: Icon.createWithBitmap(largeBitmap),
+        largeBitmap: largeBitmap,
+    };
+}
+
+
+function 下载图片() {
+
+    var url = "https://img.imimi.im/2021/11/15/2598bd5942c9a.png";
+
+    var res = http.get(url);
+    if (res.statusCode == 200) {
+        files.writeBytes("/sdcard/1.png", res.body.bytes());
+    }
+}
